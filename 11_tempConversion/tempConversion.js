6527a51542ed68ec02956ed93b395321feb435b0
joshh61 <joshh61@live.com>
2026-01-07 23:06:36 -0600

    finished 10

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  //let cel = (5/9 * (far - 32)); 
+
+  //return Math.round(cel * 10) / 10;
+
+  return Math.round((5/9 * (far - 32) * 10)) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  //let far = (9/5 * cel) + 32;
+
+  return Math.round((((9/5 * cel) + 32) * 10)) / 10;
 };
 
 // Do not edit below this line
