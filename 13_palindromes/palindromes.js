0d44de9f139fd6491524da4f0a58fd860ab6f25c
joshh61 <joshh61@live.com>
2026-01-14 18:55:17 -0600

    finished palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let lowerCased = string.toLowerCase();
 
+    lowerCased = lowerCased.replace(/[^a-zA-Z0-9]/g, "");
+
+    for(let i = 0; i < lowerCased.length; i++){
+        if(lowerCased.at(i) !== lowerCased.at((lowerCased.length - 1) - i)){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
