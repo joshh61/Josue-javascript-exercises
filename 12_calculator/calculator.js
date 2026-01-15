996d8ee859bb35d7ccd0be9a1e43350ff6af1c1a
joshh61 <joshh61@live.com>
2026-01-14 18:30:40 -0600

    finished calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, item) => acc + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, item) => acc * item, 1)
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+ return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let product = 1;
+
+  for(let i = a; i > 0; i--){
+    product *= i;
+  }
+
+  return product;
 };
 
 // Do not edit below this line
