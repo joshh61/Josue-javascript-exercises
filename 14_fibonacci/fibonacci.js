2259db1a50000cec99f322af17cd6a116aaaf7f7
joshh61 <joshh61@live.com>
2026-01-14 19:19:28 -0600

    finished fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(a < 0){
+        return "OOPS";
+    }
+    const goldenRatio = 1.618034;
+    const fib = ((goldenRatio ** a) - (1 - goldenRatio)) / Math.sqrt(5);
+    return Math.floor(fib);
 };
 
 // Do not edit below this line
